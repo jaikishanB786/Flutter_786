a1f42e0358033c485ae394f7ac9efc338d2aad4f
JaikishanBairwa <jaikishanb99@gmail.com>
2022-08-02 15:13:01 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(myApp());
 }
 
 class myApp extends StatelessWidget {
-  const myApp({Key? key}) : super(key: key);
+  // const myApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     // double pi = 3.14;
     // bool isMale = true;
     // num temp = 30.5;
@@ -20,7 +20,21 @@ class myApp extends StatelessWidget {
     // final;
 
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        // primarySwatch: Colors.red,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage(),
+      },
     );
   }
 }
