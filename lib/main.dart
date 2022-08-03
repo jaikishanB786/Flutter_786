f263ae666219ae9248721d47b6bfe08584454e40
JaikishanBairwa <jaikishanb99@gmail.com>
2022-08-03 14:57:57 +0530

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(myApp());
 }
 
 class myApp extends StatelessWidget {
-  const myApp({Key? key}) : super(key: key);
+  // const myApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     // double pi = 3.14;
     // bool isMale = true;
     // num temp = 30.5;
@@ -20,7 +22,24 @@ class myApp extends StatelessWidget {
     // final;
 
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      // debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        // primarySwatch: Colors.red,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/":(context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute :(context) => LoginPage(),
+      },
     );
   }
 }
